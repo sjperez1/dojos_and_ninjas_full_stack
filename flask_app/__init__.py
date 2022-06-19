0ca2fbbdd7013f60fa8a15d6058e90d267127e4a
sjperez1 <sarahpz479@gmail.com>
2022-06-18 19:13:43 -0600

    updated comments

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-# If we were using session in users,py, we would add the following line here
+# If we were using session in contollers, we would add the following line here
 app.secret_key = "shhhhhh"
 
 # The following allows easy changing of database name throughout our files
